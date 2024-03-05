65232a4e000ab42082969b4417326d40c342c246
Francesco Cri <crtfnc90p27c352v@studenti.unical.it>
2024-03-05 19:11:00 +0100

    Delete commit (complete (maybe))

--- project_/security_project_bkn/src/main/java/com/example/security/error_class/InvalidAuthTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.example.security.error_class;
-
-public class InvalidAuthTokenException extends RuntimeException {
-    public InvalidAuthTokenException(String message){
-        super(message);
-    }
-}
--- project_/security_project_bkn/src/main/java/com/example/security/error_class/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.security.error_class;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message){
-        super(message);
-    }
-}
